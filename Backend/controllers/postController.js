8c0f2efa8b1d814f1e5e155099bd81a94df58a19
hamza-sabat <117444355+hamza-sabat@users.noreply.github.com>
2024-12-18 17:32:39 +0000

    express server setup and backend file structure

---
+++ Backend/controllers/postController.js
@@ -0,0 +1,7 @@
+exports.getAllPosts = async (req, res) => {  
+    // Logic to get all posts  
+  };  
+  
+  exports.createPost = async (req, res) => {  
+    // Logic to create a new post  
+  };  
---
+++ Backend/controllers/userController.js
@@ -0,0 +1,8 @@
+exports.register = async (req, res) => {  
+    // Registration logic  
+  };  
+  
+  exports.login = async (req, res) => {  
+    // Login logic  
+  };  
+  
