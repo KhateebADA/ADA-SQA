cd27019c09167e90301309ba50df3753d4e48250
hamza-sabat <117444355+hamza-sabat@users.noreply.github.com>
2024-12-19 14:38:45 +0000

    revised frontend and backend file structure

--- Backend/controllers/postController.js
+++
@@ -1,7 +0,0 @@
-exports.getAllPosts = async (req, res) => {  
-    // Logic to get all posts  
-  };  
-  
-  exports.createPost = async (req, res) => {  
-    // Logic to create a new post  
-  };  
--- Backend/controllers/userController.js
+++
@@ -1,8 +0,0 @@
-exports.register = async (req, res) => {  
-    // Registration logic  
-  };  
-  
-  exports.login = async (req, res) => {  
-    // Login logic  
-  };  
-  
